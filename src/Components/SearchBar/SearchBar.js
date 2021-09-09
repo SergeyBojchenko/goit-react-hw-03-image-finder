4f2cbbfadcffa0a9775fe856e27f5433839e357c
Sergey Bojchenko <72376901+SergeyBojchenko@users.noreply.github.com>
2021-09-09 03:55:23 +0300

    DZ_3 image finder

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import styles from './SearchBar.module.css';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = e => {
+    this.setState({ query: e.currentTarget.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.query);
+    this.setState({ query: '' });
+  };
+
+  render() {
+    return (
+      <header className={styles.SearchBar}>
+        <form className={styles.SearchForm} onSubmit={this.handleSubmit}>
+          <button type="submit" className={styles.SearchFormButton}>
+            <span className={styles.SearchFormButtonLabel}>Search</span>
+          </button>
+
+          <input
+            value={this.state.query}
+            className={styles.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
+
